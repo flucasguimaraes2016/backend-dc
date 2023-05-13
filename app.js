8b48b4b89a11465d25321d9926298151443422af
salesfelipe47 <salesfelipe47@gmail.com>
2023-05-13 15:59:24 -0300

    sdfg

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var carroCompras = require("./routes/carrinhos")
 var app = express();
 
 var express = require('express')
+var cors = require('cors')
 app.user(cors())
 
 const db = require('./db')
